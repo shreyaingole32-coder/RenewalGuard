28dc2fbd881ed41824baf990dbc80306e086889f
SHREYA INGOLE <shreyaingole32@gmail.com>
2026-08-20 22:19:39 +0530

    Update ContractStatus.java

--- apps/api/src/main/java/com/renewalguard/domain/enums/ContractStatus.java
+++ apps/api/src/main/java/com/renewalguard/domain/enums/ContractStatus.java
@@ -1,1 +1,12 @@
+package com.renewalguard.domain.enums;
 
+
+public enum ContractStatus {
+    ACTIVE,
+    RENEWAL_UPCOMING,
+    NEGOTIATION_IN_PROGRESS,
+    PENDING_APPROVAL,
+    RENEWED,
+    CANCELLED,
+    EXPIRED
+}
