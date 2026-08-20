478ce07cece157d0a8230097653cabbd48225eb4
SHREYA INGOLE <shreyaingole32@gmail.com>
2026-08-20 22:52:24 +0530

    Update outputs.tf

--- infrastructure/terraform/modules/eks/outputs.tf
+++ infrastructure/terraform/modules/eks/outputs.tf
@@ -1,1 +1,20 @@
 
+output "db_instance_id" {
+  value = aws_db_instance.this.id
+}
+
+output "db_endpoint" {
+  value = aws_db_instance.this.endpoint
+}
+
+output "db_address" {
+  value = aws_db_instance.this.address
+}
+
+output "db_port" {
+  value = aws_db_instance.this.port
+}
+
+output "db_name" {
+  value = aws_db_instance.this.db_name
+}
