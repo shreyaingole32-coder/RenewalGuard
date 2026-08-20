a83f6b296f7b691ceee140be9eb6821b01420f1a
SHREYA INGOLE <shreyaingole32@gmail.com>
2026-08-20 22:10:27 +0530

    Update OpenApiConfig.java

--- apps/api/src/main/java/com/renewalguard/config/OpenApiConfig.java
+++ apps/api/src/main/java/com/renewalguard/config/OpenApiConfig.java
@@ -1,1 +1,22 @@
 
+package com.renewalguard.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI renewalGuardOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RenewalGuard API")
+                        .description(
+                                "AI-powered SaaS renewal and procurement management API"
+                        )
+                        .version("v1.0.0"));
+    }
+}
