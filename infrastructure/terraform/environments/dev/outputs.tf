aeb86100cccc82bb2fe3aa498cc1cf5ebe797b55
SHREYA INGOLE <shreyaingole32@gmail.com>
2026-08-20 22:55:02 +0530

    Create outputs.tf

---
+++ infrastructure/terraform/environments/dev/outputs.tf
@@ -0,0 +1,27 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "rds_endpoint" {
+  value = module.rds.db_endpoint
+}
+
+output "s3_bucket_name" {
+  value = module.s3.bucket_name
+}
