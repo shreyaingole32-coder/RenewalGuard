d35847ff5cf106783ae60661d3faffda383d8916
SHREYA INGOLE <shreyaingole32@gmail.com>
2026-08-20 22:52:08 +0530

    Update variables.tf

--- infrastructure/terraform/modules/eks/variables.tf
+++ infrastructure/terraform/modules/eks/variables.tf
@@ -1,1 +1,71 @@
 
+variable "name" {
+  type = string
+}
+
+variable "db_identifier" {
+  type = string
+}
+
+variable "postgres_version" {
+  type    = string
+  default = "16"
+}
+
+variable "instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "allocated_storage" {
+  type    = number
+  default = 20
+}
+
+variable "max_allocated_storage" {
+  type    = number
+  default = 100
+}
+
+variable "database_name" {
+  type    = string
+  default = "renewalguard"
+}
+
+variable "database_username" {
+  type = string
+}
+
+variable "database_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "multi_az" {
+  type    = bool
+  default = false
+}
+
+variable "backup_retention_period" {
+  type    = number
+  default = 7
+}
+
+variable "skip_final_snapshot" {
+  type    = bool
+  default = true
+}
+
+variable "deletion_protection" {
+  type    = bool
+  default = false
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
