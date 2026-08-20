b719bdf2ae3fdd5dcb5be8c35f1863b6262bcabf
SHREYA INGOLE <shreyaingole32@gmail.com>
2026-08-20 22:24:47 +0530

    Update GenerateRenewalRequest.java

--- apps/api/src/main/java/com/renewalguard/dto/GenerateRenewalRequest.java
+++ apps/api/src/main/java/com/renewalguard/dto/GenerateRenewalRequest.java
@@ -1,1 +1,9 @@
+package com.renewalguard.dto;
 
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record GenerateRenewalRequest(
+        @NotNull UUID contractId
+) {}
