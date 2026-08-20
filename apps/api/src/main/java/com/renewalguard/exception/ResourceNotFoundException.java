b4fac256985164434bf558cac9c2bd47acd60f15
SHREYA INGOLE <shreyaingole32@gmail.com>
2026-08-20 22:26:34 +0530

    Update ResourceNotFoundException.java

--- apps/api/src/main/java/com/renewalguard/exception/ResourceNotFoundException.java
+++ apps/api/src/main/java/com/renewalguard/exception/ResourceNotFoundException.java
@@ -1,1 +1,8 @@
+package com.renewalguard.exception;
 
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
