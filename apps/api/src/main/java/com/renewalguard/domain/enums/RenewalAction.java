70b7b02dfff9b2064bfbc7c5ad82611d342e2203
SHREYA INGOLE <shreyaingole32@gmail.com>
2026-08-20 22:17:37 +0530

    Update RenewalAction.java

--- apps/api/src/main/java/com/renewalguard/domain/enums/RenewalAction.java
+++ apps/api/src/main/java/com/renewalguard/domain/enums/RenewalAction.java
@@ -1,1 +1,12 @@
+package com.renewalguard.domain.enums;
 
+public enum RenewalStatus {
+    UPCOMING,
+    IN_REVIEW,
+    PENDING_APPROVAL,
+    APPROVED,
+    REJECTED,
+    RENEWED,
+    CANCELLED,
+    EXPIRED
+}
