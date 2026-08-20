43d27a9bcf2bbe7612efe017ec8106f8b96b6e85
SHREYA INGOLE <shreyaingole32@gmail.com>
2026-08-20 22:25:14 +0530

    Update LoginRequest.java

--- apps/api/src/main/java/com/renewalguard/dto/LoginRequest.java
+++ apps/api/src/main/java/com/renewalguard/dto/LoginRequest.java
@@ -1,1 +1,9 @@
+package com.renewalguard.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @Email @NotBlank String email,
+        @NotBlank String password
+) {}
